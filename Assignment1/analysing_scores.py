00b9a41d1b5037422b6b85c9b2211c423820a187
tatu <tatu.r.makinen@gmail.com>
2019-02-01 10:17:03 +0200

    Analysing results

---
+++ Assignment1/analysing_scores.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+all_scores = np.load('all_scores.npy')
+
+def getScores(all_scores,column):
+    summary = []
+    ids = np.unique(all_scores[:,column])
+    for id in ids:
+        mask = all_scores[:,column]==id
+        glob_inds = np.where(mask)[0]
+        scores = all_scores[mask,3]
+        scores = [ float(x) for x in scores ]
+        scores = np.array(scores)
+        max_ind = np.argmax(scores)
+        best = all_scores[glob_inds[max_ind],:]
+        summary.append([id,best,scores])
+
+    printSummary(summary,column)
+    return summary
+
+def plotSummary(summary):
+    for thing in summary:
+        plt.figure()
+        plt.title("{}\nmean={:.5}\nstd={:.5}\nMax={:.5}\n({}, {}, {})".format(
+                thing[0],np.mean(thing[2]),np.std(thing[2]),thing[1][3],
+                thing[1][0],thing[1][1],thing[1][2]))
+        plt.plot(thing[2])
+        plt.plot([0,np.shape(thing[2])[0]],[np.mean(thing[2]),np.mean(thing[2])])
+        
+def printSummary(summary,ind):
+    names = ['Classifier','Filter', 'Feature']
+    print("\n\n{} Results:\n@    @    @    @".format(names[ind]))
+    best_thing = 0
+    for thing in summary:
+        if best_thing==0:
+            best_thing = thing
+        elif np.mean(thing[2])>np.mean(best_thing[2]):
+            best_thing = thing
+                
+        print()
+        print(thing[0])
+        print("Mean acc {:.5}, std {:.5}".format(np.mean(thing[2]),np.std(thing[2])))
+        print("Max acc {:.5} with {},{},{}".format(
+                thing[1][3],thing[1][0],thing[1][1],thing[1][2]))
+    print("\n=================================================")
+    print("Best {} on average:  {}\n(with {:.5} mean acc)".format(names[ind],best_thing[0],np.mean(best_thing[2])))
+    print("=================================================")
+
+
+classifier_summary = getScores(all_scores,0)
+limit_method_summary = getScores(all_scores,1)
+feature_method_summary = getScores(all_scores,2)
+
+
+plotSummary(classifier_summary)
+plotSummary(limit_method_summary)
+plotSummary(feature_method_summary)
--- Assignment1/normalClassifiers.py
+++ Assignment1/normalClassifiers.py
@@ -5,10 +5,12 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import numpy as np
+from os import getcwd
 from pandas import read_csv,to_numeric
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import GroupShuffleSplit, cross_val_score
+import numpy as np
+import csv
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.svm import SVC,LinearSVC
@@ -18,13 +20,17 @@ from sklearn.ensemble import AdaBoostClassifier as AdaB
 from sklearn.ensemble import GradientBoostingClassifier as GradB
 from sklearn.linear_model import LogisticRegression as LogR
 from sklearn.linear_model import SGDClassifier as SGDC
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.neighbors import RadiusNeighborsClassifier as RNeigh
+from sklearn.neighbors import NearestCentroid as NearC
+from sklearn.neural_network import MLPClassifier as MLPC
 
 
 #==============================================================================
 # Loading data: X, y, groups, X_test_orig
 #==============================================================================
 
-folder = 'C:\\\\Users\\makine42\\Desktop\\PatternRecognition\\robotsurface\\'
+folder = getcwd() +'/robotsurface/'
 X_test_orig = np.load(folder+'X_test_kaggle.npy')
 X = np.load(folder+'X_train_kaggle.npy')
 y_orig = read_csv(folder+'y_train_final_kaggle.csv').values
@@ -38,7 +44,7 @@ data = [X,y,groups,X_test_orig]
 # Function definitions
 #==============================================================================
 
-def makeFeatureMatrix(X,method):
+def getFeatures(X,method):
     # if 2D data, expand to 3D
     if len(np.shape(X)) == 2:
         X = np.expand_dims(X, axis=1)
@@ -91,6 +97,10 @@ def getRelevantData(X,method):
         # Acceleration as absolute values in X and Y directions
         X = X[:,7:]
         X[:,:2] = np.abs(X[:,:2])
+    elif method == 'vel_abs_acc_xy':
+        # Velocity and Acceleration (absolute values in X and Y directions)
+        X = X[:,4:]
+        X[:,3:5] = np.abs(X[:,3:5])
     else:
         raise ValueError('Unknown data limiting method')
     return X
@@ -98,29 +108,39 @@ def getRelevantData(X,method):
 def test_classifier(data,clf,limitmethod,featuremethod,n_splits):
     X = data[0]
     X = getRelevantData(X,limitmethod)
-    X_f = makeFeatureMatrix(X,featuremethod)
+    X_f = getFeatures(X,featuremethod)
     y = data[1]
     groups = data[2]
     
     cv = GroupShuffleSplit(n_splits=n_splits,test_size=0.2)
     return cross_val_score(clf,X_f,y,groups,cv=cv)
 
-def makeSubmissionFile(clf,data,le,limitmethod,featuremethod):
+def makeSubmissionFile(clf,data,le,limitmethod,featuremethod,aug_on=0):
     X = data[0]
     y = data[1]
     X_test = data[3]
-    X, X_test = getRelevantData(X, X_test,limitmethod)
-    X, X_test = makeFeatureMatrix(X,X_test,featuremethod)
-    clf.fit(X,y)
-    y_pred = clf.predict(X_test)
+    X = getRelevantData(X,limitmethod)
+    X_f = getFeatures(X,featuremethod)
+    X_test = getRelevantData(X_test,limitmethod)
+    X_test_f = getFeatures(X_test,featuremethod)
+    clf.fit(X_f,y)
+    y_pred = clf.predict(X_test_f)
+    # Use predictions to fit to test data as well
+    if aug_on == 1:
+        X_augmented = np.vstack((X_f,X_test_f))
+        y_augmented = np.hstack((y,y_pred))
+        clf.fit(X_augmented,y_augmented)
+        y_pred = clf.predict(X_test_f)
+    
     labels =list(le.inverse_transform(y_pred))
 
-    with open("submission.csv", "w") as fp:
-        fp.write("# Id,Surface\\n")
+    with open("submission.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["# Id","Surface"])
         for i, label in enumerate (labels):
-            fp.write("%d,%s\\n" % (i, label))
+            writer.writerow(["{}".format(i),"{}".format(label)])
 
-#==============================================================================
+# %%==============================================================================
 # Looping over:
 #     - data limitation methods
 #     - feature extraction methods
@@ -128,13 +148,13 @@ def makeSubmissionFile(clf,data,le,limitmethod,featuremethod):
 #==============================================================================
 
 # Control reliability of evalutation vs. speed
-n_splits = 3
+n_splits = 200
 
 # Filter data:
 # basic:    'orient','vel','acc','orient_vel','orient_acc','vel_acc','all'
-# advanced: 'abs_acc_xy'
-limit_methods = ['orient','vel','acc','orient_vel','orient_acc','vel_acc','all',
-                 'abs_acc_xy']
+# advanced: 'abs_acc_xy','vel_abs_acc_xy'
+limit_methods = ['orient','vel','acc','orient_vel','orient_acc','vel_acc','all', \
+                 'abs_acc_xy','vel_abs_acc_xy']
 
 # Extract features:
 # basic:    'mean','std','mean_std','all'
@@ -143,53 +163,37 @@ feature_methods = ['mean','std','mean_std','all']
 # Classifiers:
 # linear:   LDA(),LogR(),SGDC()
 # svm:      SVC(),LinearSVC()
-# ensemble: RFC(),Xtree(),AdaB(),GradB()
-classifiers = [LDA(),
-               LinearSVC(), # fails to converge
-               SGDC(),
-               SVC(),
-               RFC(),
-               LogR(),
-               XTree(),
-               AdaB(),
-               GradB()]
+# ensemble: RFC(),XTree(),AdaB(),GradB()
+# neighbor: KNN(),RNeigh(),NearC()
+# neural:   MLPC()
 
-print("Results\n==========================")
+
+classifiers = [LDA(),LogR(),SGDC(),SVC(),LinearSVC(),RFC(),XTree(),AdaB(), \
+                GradB(),KNN(),(),NearC(),MLPC()]
+
+clf_names = [x.__class__.__name__ for x in classifiers]
+
 all_scores = []
 for limit_method in limit_methods:
     for feature_method in feature_methods:
-        for clf in classifiers:
+        for clf,clf_name in zip(classifiers,clf_names):
             scores = test_classifier(data,clf,limit_method,feature_method,n_splits)
-            all_scores.append([clf.__class__.__name__,limit_method,feature_method,scores])
+            all_scores.append([clf_name,limit_method,feature_method,scores])
             mean_score = np.mean(scores)
             print("{:.5f} accuracy for {}, {}, {}".format(
-                mean_score,limit_method,feature_method,clf.__class__.__name__))
-
-# %%==============================================================================
-# Printing summary of results
-#==============================================================================
-all_scores = np.array(all_scores)
-print("Summary\n=============================")
-limit_sort_inds = np.argsort(all_scores[0,:])
+                mean_score,limit_method,feature_method,clf_name))
 
 
-feature_sort_inds = np.argsort(all_scores[1,:])
-
-
-# For submitting results
-"""
-chosen_clf = classifiers[np.argmax(mean_scores)]
-print("Chosen classifier is {} with mean accuracy {:.5}".format(
-        chosen_clf.__class__.__name__, np.max(mean_scores)))
-
-makeSubmissionFile(clf,data,le,limitmethod,featuremethod)
-"""
-# For saving data
-"""
+# Save data
 data = []
 for score in all_scores:
     data.append([score[0],score[1],score[2],np.mean(score[3])])
-    
 data = np.array(data)
-np.save('all_scores',data)
-"""
+np.save('new_scores',data)
+
+
+#%% For submitting results
+clf = RFC(n_estimators = 100)
+limitmethod = 'vel_abs_acc_xy'
+featuremethod = 'mean_std'
+makeSubmissionFile(clf,data,le,limitmethod,featuremethod,1)
